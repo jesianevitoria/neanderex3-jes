a94723434a57f0b3e09d2c1009bb99f18dcdcac8
jesianevitoria <jesiane.vitoria@souunit.com.br>
2025-09-18 02:27:14 -0300

    sobre:
    O programa começa lendo dois números digitados pelo usuário. O primeiro número é lido com o comando IN e armazenado na posição de memória chamada NUM1 através do comando STO NUM1. Em seguida, o segundo número é lido da mesma forma e armazenado em NUM2.
    
    Depois de armazenar os números, o programa carrega o primeiro número no acumulador usando LDA NUM1. Em seguida, ele soma o segundo número ao valor do acumulador com o comando ADD NUM2. O resultado dessa soma é exibido na tela usando o comando OUT.
    
    Por fim, o programa termina sua execução com HLT. As posições de memória NUM1 e NUM2 são definidas com o comando DAT, que serve apenas para reservar espaço para os números que serão armazenados.
    
    Em resumo, este código lê dois números do usuário, calcula a soma entre eles e mostra o resultado na tela.

---
+++ neander.asm
@@ -0,0 +1,13 @@
+        LDA N1
+        ADD N2
+        STA SUM
+        LDA N1
+        ADD N1
+        ADD N1
+        STA PROD
+        HLT
+
+N1      DEC 2
+N2      DEC 3
+SUM     DEC 0
+PROD    DEC 0
